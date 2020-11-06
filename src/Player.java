1dfad539985db6fa46238bfaeda135fbcbcadb9f
Konstantinos Papageorgiadis <papchakon@csd.auth.gr>
2020-11-06 15:55:30 +0200

    fixed scanner2

--- src/Player.java
+++ src/Player.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Player {
 
     private String username;
+    private int score;
 
     public Player(String username){
 
         this.username=username;
+        this.score=0;
 
     }
 

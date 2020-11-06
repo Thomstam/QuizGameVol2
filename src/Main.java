3c5ae3aee1be95e6018527f31079bca9af7ca7d4
Thomas <thomstam@csd.auth.gr>
2020-11-06 11:44:16 +0200

    Prototype of reading a file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, let's play Buzz!");
+
+        List<List<String>> temp;
+        temp = ReadingFromFile.fillingTheData("Math.txt");
+        for (int i = 0; i < temp.size(); i++) {
+            for (int j = 0; j < temp.get(i).size(); j++) {
+                System.out.println(temp.get(i).get(j) + " ");
+            }
+        }
     }
 }

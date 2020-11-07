98e49c33afaa34ddf5f4cda6a011d9045143ab17
Thomas <thomstam@csd.auth.gr>
2020-11-07 17:23:31 +0200

    Betting implementation

---
+++ src/Betting.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Betting extends Gamemode{
+
+    @Override
+    void gamemodePlay(ArrayList<Player> players) {
+
+    }
+
+    @Override
+    void handleTheScore(Player player) {
+
+    }
+}

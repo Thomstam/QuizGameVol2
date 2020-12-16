4f0ed4d4159a56da953f2140592acd127c4e2ef6
Konstantinos Papageorgiadis <papchakon@csd.auth.gr>
2020-12-16 22:52:17 +0200

    fixed FastestWins

--- src/MainPackage/Gamemodes/FastestWins.java
+++ src/MainPackage/Gamemodes/FastestWins.java
@@ -1,15 +1,23 @@
 package MainPackage.Gamemodes;
 
+import MainPackage.Controllers.GameOptionsController;
 import MainPackage.Player;
 import MainPackage.Question;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class FastestWins extends Gamemode {
 
     @Override
     void handleTheScore(ArrayList<Player> players, Question question) {
-        boolean someoneWasAlreadyCorrect= false;
+        boolean swapped=false;
+        if(players.get(1).getPlacement().equals("First")){
+            Collections.swap(players,0,1);
+            swapped=true;
+        }
+
         for (Player player: players) {
             int indexOfChoice;
             switch (player.getPlacement()){
@@ -18,32 +26,24 @@ public class FastestWins extends Gamemode {
                     if (question.getPossibleAnswersToAsk().get(indexOfChoice).equals(question.getCorrectAnswer())){
                         player.setScore(player.getScore() + 1000);
                         player.setIsTheAnswerCorrect(true);
-                        someoneWasAlreadyCorrect = true;
                     }else{
                         player.setIsTheAnswerCorrect(false);
+                        GameOptionsController.userController.listOfPlayers().get(1).setPlacement("First");
                     }
                     break;
                 case "Second":
-                    if (someoneWasAlreadyCorrect){
-                        indexOfChoice = handlePlayerChoice(player.getAnswer());
-                        if (question.getPossibleAnswersToAsk().get(indexOfChoice).equals(question.getCorrectAnswer())){
-                            player.setScore(player.getScore() + 500);
-                            player.setIsTheAnswerCorrect(true);
-                        }else{
-                            player.setIsTheAnswerCorrect(false);
-                        }
-                    }else {
-                        indexOfChoice = handlePlayerChoice(player.getAnswer());
-                        if (question.getPossibleAnswersToAsk().get(indexOfChoice).equals(question.getCorrectAnswer())){
-                            player.setScore(player.getScore() + 1000);
-                            player.setIsTheAnswerCorrect(true);
-                        }else{
-                            player.setIsTheAnswerCorrect(false);
-                        }
+                    indexOfChoice = handlePlayerChoice(player.getAnswer());
+                    if (question.getPossibleAnswersToAsk().get(indexOfChoice).equals(question.getCorrectAnswer())){
+                        player.setScore(player.getScore() + 500);
+                        player.setIsTheAnswerCorrect(true);
+                    }else{
+                        player.setIsTheAnswerCorrect(false);
                     }
-
-
             }
         }
+        if(swapped==true){
+            Collections.swap(players,0,1);
+        }
+
     }
 }

c29e0ab22f5cf39b517e2ce4ebdc548316c4118a
Thomas <thomstam@csd.auth.gr>
2020-12-16 22:24:32 +0200

    score fixing

--- src/MainPackage/Gamemodes/FastestWins.java
+++ src/MainPackage/Gamemodes/FastestWins.java
@@ -9,7 +9,6 @@ public class FastestWins extends Gamemode {
 
     @Override
     void handleTheScore(ArrayList<Player> players, Question question) {
-        boolean someoneWasAlreadyCorrect= false;
         for (Player player: players) {
             int indexOfChoice;
             switch (player.getPlacement()){
@@ -18,32 +17,21 @@ public class FastestWins extends Gamemode {
                     if (question.getPossibleAnswersToAsk().get(indexOfChoice).equals(question.getCorrectAnswer())){
                         player.setScore(player.getScore() + 1000);
                         player.setIsTheAnswerCorrect(true);
-                        someoneWasAlreadyCorrect = true;
                     }else{
                         player.setIsTheAnswerCorrect(false);
                     }
                     break;
                 case "Second":
-                    if (someoneWasAlreadyCorrect){
-                        indexOfChoice = handlePlayerChoice(player.getAnswer());
-                        if (question.getPossibleAnswersToAsk().get(indexOfChoice).equals(question.getCorrectAnswer())){
-                            player.setScore(player.getScore() + 500);
-                            player.setIsTheAnswerCorrect(true);
-                        }else{
-                            player.setIsTheAnswerCorrect(false);
-                        }
-                    }else {
-                        indexOfChoice = handlePlayerChoice(player.getAnswer());
-                        if (question.getPossibleAnswersToAsk().get(indexOfChoice).equals(question.getCorrectAnswer())){
-                            player.setScore(player.getScore() + 1000);
-                            player.setIsTheAnswerCorrect(true);
-                        }else{
-                            player.setIsTheAnswerCorrect(false);
-                        }
+                    indexOfChoice = handlePlayerChoice(player.getAnswer());
+                    if (question.getPossibleAnswersToAsk().get(indexOfChoice).equals(question.getCorrectAnswer())){
+                        player.setScore(player.getScore() + 500);
+                        player.setIsTheAnswerCorrect(true);
+                    }else{
+                        player.setIsTheAnswerCorrect(false);
                     }
-
-
             }
+
         }
     }
 }
+

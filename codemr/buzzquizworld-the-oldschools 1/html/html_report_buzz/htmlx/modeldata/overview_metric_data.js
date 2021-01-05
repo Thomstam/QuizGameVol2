269d7b1c939ede068ab11d4a76c126d6c60d16d6
Thomas <thomstam@csd.auth.gr>
2021-01-05 12:55:00 +0200

    Necessary things From Intellij

---
+++ codemr/buzzquizworld-the-oldschools 1/html/html_report_buzz/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"buzzquizworld-the-oldschools working set: {Buzz}", "Number of Highly Problematic Classes":0, "Number of Entities":23, "Number of Problematic Classes":0, "Class Lines of Code":965, "Number of External Packages":9, "Number of Packages":3, "Number of External Entities":42}
---
+++ codemr/buzzquizworld-the-oldschools 1/html/html_report_buzz/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,11 @@
+var matrix = [[0,1,0],[6,0,2],[6,3,0]]
+var packages = [{
+"name": " MainPackage", "color": " #3182bd"
+}
+,{
+"name": " MainPackage.Gamemodes", "color": " #6baed6"
+}
+,{
+"name": " MainPackage.Controllers", "color": " #9ecae1"
+}
+];
